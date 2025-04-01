f0b918bead498da22d65e556c89b7366710974bb
Otavio <estudosprogotavio@gmail.com>
2025-04-01 03:36:29 -0300

    Adicionando Repositorio a Entidade Livros

---
+++ src/main/java/dev/Zerphyis/library/Repository/BooksRepository.java
@@ -0,0 +1,9 @@
+package dev.Zerphyis.library.Repository;
+
+import dev.Zerphyis.library.Entity.Books.Books;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BooksRepository extends JpaRepository<Books,Long> {
+}
