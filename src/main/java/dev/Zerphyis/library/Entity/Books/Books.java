e7cec8e8073e9a8b1a1bcfde2c07bfff304b5c8f
Otavio <estudosprogotavio@gmail.com>
2025-04-01 03:36:19 -0300

    Adicionando  a Entidade Livros

---
+++ src/main/java/dev/Zerphyis/library/Entity/Books/Books.java
@@ -0,0 +1,94 @@
+package dev.Zerphyis.library.Entity.Books;
+
+import dev.Zerphyis.library.Entity.Author.Author;
+import dev.Zerphyis.library.Entity.Datas.DataBooksEntry;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "livros")
+public class Books {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotBlank
+    private String title;
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "autor_id")
+    private Author author;
+    @PastOrPresent
+    private LocalDate publicationDate;
+    @NotBlank
+    private String publisher;
+    @NotBlank
+    private String gender;
+    @NotNull
+    private Integer quantityAvailable;
+
+    public Books(){
+
+    }
+
+    public Books(String title, LocalDate publicationDate, String publisher, String gender, Integer quantityAvailable, Author author) {
+        this.title = title;
+        this.publicationDate = publicationDate;
+        this.publisher = publisher;
+        this.gender = gender;
+        this.quantityAvailable = quantityAvailable;
+        this.author = author;
+    }
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
+    public LocalDate getPublicationDate() {
+        return publicationDate;
+    }
+
+    public void setPublicationDate(LocalDate publicationDate) {
+        this.publicationDate = publicationDate;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Integer getQuantityAvailable() {
+        return quantityAvailable;
+    }
+
+    public void setQuantityAvailable(Integer quantityAvailable) {
+        this.quantityAvailable = quantityAvailable;
+    }
+}
