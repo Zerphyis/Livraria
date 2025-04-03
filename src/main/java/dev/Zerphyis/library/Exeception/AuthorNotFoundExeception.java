176f1e5bb97c1bcfbc8356fa189c6fb23856bf22
Otavio <estudosprogotavio@gmail.com>
2025-04-03 06:08:17 -0300

    Adicionando a exeception personalizada a Autores

---
+++ src/main/java/dev/Zerphyis/library/Exeception/AuthorNotFoundExeception.java
@@ -0,0 +1,7 @@
+package dev.Zerphyis.library.Exeception;
+
+public class AuthorNotFoundExeception extends RuntimeException {
+    public AuthorNotFoundExeception(String message) {
+        super(message);
+    }
+}
