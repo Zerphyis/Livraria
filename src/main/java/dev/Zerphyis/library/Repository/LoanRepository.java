f0b3471d1c2ca78e3e5926180fad0011acb2328f
Otavio <estudosprogotavio@gmail.com>
2025-04-03 14:40:11 -0300

    Adicionando anotation @Repository no repositorio de Emprestimos

--- src/main/java/dev/Zerphyis/library/Repository/LoanRepository.java
+++ src/main/java/dev/Zerphyis/library/Repository/LoanRepository.java
@@ -2,6 +2,8 @@ package dev.Zerphyis.library.Repository;
 
 import dev.Zerphyis.library.Entity.Loan.Loan;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface LoanRepository extends JpaRepository<Loan,Long> {
 }
